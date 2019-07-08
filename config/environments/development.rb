ee15b729dfeef2f4b282133f3914b00440a02410
Xavier Llamas <xavierll@cdm.com.mx>
2019-07-08 13:21:29 -0500

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
-    domain: Rails.application.sectrets.domain_name,
+    domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
